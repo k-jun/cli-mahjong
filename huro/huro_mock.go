ffb70755ef8ec962b9800165b2e6a230d9f9f43f
k-jun <keijun091221@gmail.com>
2021-01-06 01:24:37 +0900

    feat(huro): create mock file

---
+++ huro/huro_mock.go
@@ -0,0 +1,34 @@
+package huro
+
+import "mahjong/hai"
+
+var _ Huro = &HuroMock{}
+
+type HuroMock struct {
+	ErrorMock error
+	PonMock   [3]*hai.Hai
+	ChiMock   [3]*hai.Hai
+	KanMock   [4]*hai.Hai
+}
+
+func (h *HuroMock) Pon(hais [3]*hai.Hai) error {
+	h.PonMock = hais
+
+	return h.ErrorMock
+}
+
+func (h *HuroMock) Chi(hais [3]*hai.Hai) error {
+	h.ChiMock = hais
+	return h.ErrorMock
+}
+
+func (h *HuroMock) Kan(hais [4]*hai.Hai) error {
+	h.KanMock = hais
+	return h.ErrorMock
+}
+
+func (h *HuroMock) Kakan(x *hai.Hai) error {
+	h.KanMock = [4]*hai.Hai{h.PonMock[0], h.PonMock[1], h.PonMock[2], x}
+	h.PonMock = [3]*hai.Hai{}
+	return h.ErrorMock
+}
